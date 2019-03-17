a64879b7c4bdb2805ac38360376d17f57402d652
Sanchit Sinha <sinhasunny1979@gmail.com>
2019-03-17 14:52:20 -0700

    Fixed shakespeare

--- Shakespeare/shakespeare.py
+++ Shakespeare/shakespeare.py
@@ -5,9 +5,8 @@ import os
 
 nltk.download('stopwords')
 
-folder = "./works"
+folder = "."
 sub = "[^a-zA-Z' ]+"
-
 
 
 def uniqueWords(file_name):
@@ -26,16 +25,20 @@ def uniqueWords(file_name):
 			if word in unique_set:
 				count_dict[word] += 1
 
-	out_name = "works_unique_words/" + file_name + "_unique_words"
+	out_name = file_name + "_unique_words"
 	print(file_name, "has", len(unique_set), "words!")
-	out_file = open(out_name, "w+")
+	out_file = open(out_name, "w")
 	for word in unique_set:
 		out_file.write("{} {}\n".format(word, count_dict[word]))
 	out_file.close()
 	file.close()
 
 
+# i = 0
+# for root, dirs, files in os.walk(folder):
+# 	for f in files:
+# 		print(i)
+# 		i+= 1
 
-for root, dirs, files in os.walk(folder):
-	for f in files:
-		uniqueWords(f)
+if __name__ == "__main__":
+	uniqueWords("Shakespeare_all_works")
